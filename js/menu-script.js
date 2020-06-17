fec50a907856d8710627ce1a8bb283158162889a
GEAR-2012 <sandor.tudja@gmail.com>
2020-06-17 20:24:13 +0100

    css update ...

--- js/menu-script.js
+++ js/menu-script.js
@@ -1,4 +1,4 @@
-/* Last modified 14 Jun 2020 by Sandor Tudja in Kemnay, Scotland */
+/* Last modified 16 Jun 2020 by Sandor Tudja in Kemnay, Scotland */
 /* © Gear Web development 2020 */
 const getMenuBtn = document.querySelector(".container-menu-btn");
 const getMenuItems = document.querySelectorAll(".menu-item");
@@ -7,48 +7,66 @@ const screenWidth = window.outerWidth;
 const menuAnimSpeed = 0.3; //animation speed in seconds
 const menuStiffness = 8; //change the number bigger to more stiffness
 const itemAnimDelay = menuAnimSpeed / menuStiffness; // item anim delay increment
-var fullAnimTime = menuAnimSpeed + itemAnimDelay * getMenuItems.length;
+let fullAnimTime = menuAnimSpeed + itemAnimDelay * getMenuItems.length;
 let menuSwitch = false; //Is menu on?
 let hideTimer;
 let delayTimer;
 
+// calculate the hidden state of the menu items (transleteX amount)
+function setMenuSlideAmount() {
+    let x; // hidden state of the menu items (transleteX amount)
+    if (screenWidth < 768) {
+        // mobile
+        x = 160;
+    } else if (screenWidth < 1024) {
+        // small tablet
+        x = 250;
+    } else if (screenWidth < 1336) {
+        // large tablet
+        x = 300;
+    }
+    return x;
+}
+
 // mobile portrait & landscape & tablet portrait
 if (screenWidth <= 1024) {
-  setUpMenu();
+    setUpMenu();
 }
 
 function setUpMenu() {
-  getMenuBtn.addEventListener("click", menuToggle);
-  getMenu.classList.add("no-show");
-  // set menu items to animate
-  getMenuItems.forEach((menuItem, i) => {
-    menuItem.addEventListener("click", menuToggle);
-    menuItem.style.transition = `all ${menuAnimSpeed}s ${
-      (i + 1) * itemAnimDelay
-    }s`;
-  });
+    getMenuBtn.addEventListener("click", menuToggle);
+    getMenu.classList.add("no-show");
+    // set menu items to animate
+    getMenuItems.forEach((menuItem, i) => {
+        menuItem.addEventListener("click", menuToggle);
+        menuItem.style.transition = `all ${menuAnimSpeed}s ${
+            (i + 1) * itemAnimDelay
+        }s`;
+    });
 }
 
 function menuToggle() {
-  let moveX = 15;
-  clearTimeout(hideTimer);
-  clearTimeout(delayTimer);
-  if (menuSwitch === false) {
-    // show
-    getMenu.classList.remove("no-show");
-    menuSwitch = true;
-  } else if (menuSwitch === true) {
-    // hide
-    hideTimer = setTimeout(() => {
-      getMenu.classList.add("no-show");
-    }, fullAnimTime * 1000);
-    moveX = 210;
-    menuSwitch = false;
-  }
-  // Pull in & out all menu items
-  delayTimer = setTimeout(() => {
-    getMenuItems.forEach((menuItem) => {
-      menuItem.style.transform = `skewY(-3deg) translateX(${moveX}px)`;
-    });
-  }, 10);
+    let slideFrom = setMenuSlideAmount(); // hidden state of the menu items (transleteX amount)
+    let slideTo = 15; // unhidden state of the menu items (transleteX amount)
+    let moveX = slideTo; // actual state of the menu items (transleteX amount)
+    clearTimeout(hideTimer);
+    clearTimeout(delayTimer);
+    if (menuSwitch === false) {
+        // show
+        getMenu.classList.remove("no-show");
+        menuSwitch = true;
+    } else if (menuSwitch === true) {
+        // hide
+        hideTimer = setTimeout(() => {
+            getMenu.classList.add("no-show");
+        }, fullAnimTime * 1000);
+        moveX = slideFrom;
+        menuSwitch = false;
+    }
+    // Pull in & out all menu items
+    delayTimer = setTimeout(() => {
+        getMenuItems.forEach((menuItem) => {
+            menuItem.style.transform = `skewY(-3deg) translateX(${moveX}px)`;
+        });
+    }, 10);
 }
--- js/price-list_button-script.js
+++ js/price-list_button-script.js
@@ -1,37 +1,44 @@
+/* Last modified 17 Jun 2020 by Sandor Tudja in Kemnay, Scotland */
+/* © Gear Web development 2020 */
+// get DOM elements
+// buttons
 let btnPricelistInit = document.querySelector(".btn.pricelist.init");
 let btnPricelistRef = document.querySelector(".btn.pricelist.ref");
+// tables
 let tablePricelistInit = document.querySelector(".table.pricelist.init");
 let tablePricelistRef = document.querySelector(".table.pricelist.ref");
-let btnSwitch2 = true;//so init active
-let x2 = window.innerWidth;
+// hr-s
+let hr1 = document.querySelector(".hr__1");
+// variables
+let btnSwitch2 = true; //so init table active
+const screenW = window.outerWidth;
 
-if(x2 >= 1024) {
-//settings to init active
-btnPricelistInit.style.opacity = "1";
-btnPricelistRef.style.opacity = ".5";
-tablePricelistInit.style.display = "table";
-tablePricelistRef.style.display = "none";
-btnPricelistRef.addEventListener("click", priceListSelector);
-}
-
-
-
-function priceListSelector() {
-  if(btnSwitch2 === true) {
-    btnPricelistInit.style.opacity = ".5";
-    btnPricelistRef.style.opacity = "1";
-    tablePricelistInit.style.display = "none";
-    tablePricelistRef.style.display = "table";
-    btnPricelistInit.addEventListener("click", priceListSelector);
-    btnPricelistRef.removeEventListener("click", priceListSelector);
-    btnSwitch2 = false;
-  } else if(btnSwitch2 === false) {
+if (screenW >= 568) {
+    //settings to init table active
     btnPricelistInit.style.opacity = "1";
     btnPricelistRef.style.opacity = ".5";
     tablePricelistInit.style.display = "table";
     tablePricelistRef.style.display = "none";
     btnPricelistRef.addEventListener("click", priceListSelector);
-    btnPricelistInit.removeEventListener("click", priceListSelector);
-    btnSwitch2 = true;
-  }
+    hr1.classList.add("hide");
+}
+
+function priceListSelector() {
+    if (btnSwitch2 === true) {
+        btnPricelistInit.style.opacity = ".5";
+        btnPricelistRef.style.opacity = "1";
+        tablePricelistInit.style.display = "none";
+        tablePricelistRef.style.display = "table";
+        btnPricelistInit.addEventListener("click", priceListSelector);
+        btnPricelistRef.removeEventListener("click", priceListSelector);
+        btnSwitch2 = false;
+    } else if (btnSwitch2 === false) {
+        btnPricelistInit.style.opacity = "1";
+        btnPricelistRef.style.opacity = ".5";
+        tablePricelistInit.style.display = "table";
+        tablePricelistRef.style.display = "none";
+        btnPricelistRef.addEventListener("click", priceListSelector);
+        btnPricelistInit.removeEventListener("click", priceListSelector);
+        btnSwitch2 = true;
+    }
 }
