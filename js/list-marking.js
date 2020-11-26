1f047c41022ef9916c2f976aff6eed461d1ec10c
GEAR-2012 <sandor.tudja@gmail.com>
2020-11-26 16:20:30 +0000

    version 2.9 corrections

--- js/list-marking.js
+++ js/list-marking.js
@@ -1,10 +1,10 @@
 /*
-Last modified 27 Jun 2020 by Sandor Tudja in Kemnay, Scotland
+Last modified 26 Nov 2020 by Sandor Tudja in Kemnay, Scotland
 © Gear Web development 2020
 */
 
 // variables
-const unavailablePerfumes = []; // perfume number list of unavailable perfumes
+const unavailablePerfumes = [115, 129, 142, 157, 178, 183, 202, 226, 257, 279, 297]; // perfume number list of unavailable perfumes
 const newPerfumes = [112, 139, 150, 178, 179, 180, 181, 296, 297]; // perfume number list of new perfumes
 const onSalePerfumes = []; // perfume number list of perfumes on sale
 
@@ -18,15 +18,6 @@ const rows = document.querySelectorAll("table.table.parlist tr");
 
 // marking rows in the perfume list
 for (let row of rows) {
-    unavailablePerfumes.forEach((perfNo) => {
-        if (row.firstElementChild.innerText == perfNo) {
-            row.classList.add("unavailable");
-            row.setAttribute(
-                "title",
-                "This perfume Currently does not available."
-            );
-        }
-    });
     newPerfumes.forEach((perfNo) => {
         if (row.firstElementChild.innerText == perfNo) {
             row.classList.add("new");
@@ -37,6 +28,13 @@ for (let row of rows) {
         if (row.firstElementChild.innerText == perfNo) {
             row.classList.add("sale");
             row.setAttribute("title", "This perfume is on sale.");
+        }
+    });
+    unavailablePerfumes.forEach((perfNo) => {
+        if (row.firstElementChild.innerText == perfNo) {
+            row.className = ""; //clear all other class names (because interference)
+            row.classList.add("unavailable");
+            row.setAttribute("title", "This perfume Temporarily does not available.");
         }
     });
 }
