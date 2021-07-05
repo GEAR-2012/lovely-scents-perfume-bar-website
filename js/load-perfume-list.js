43c94b372cb22e3d10ea250326b4996322f17ce0
GEAR-2012 <sandor.tudja@gmail.com>
2021-07-05 15:35:47 +0100

    modifying perfume list no longer available & shut down covid warning

--- js/load-perfume-list.js
+++ js/load-perfume-list.js
@@ -5,11 +5,11 @@ Last modified 15 Dec 2020 by Sandor Tudja in Kemnay, Scotland
 
 // variables
 // perfume number list of unavailable perfumes:
-const unavailablePerfumes = [129, 148, 202];
+const unavailablePerfumes = [101, 129, 148, 155, 202, 279];
 // perfume number list of new perfumes:
 const newPerfumes = [109, 126, 144, 287, 294];
 // perfume number list of perfumes on sale:
-const onSalePerfumes = [];
+const onSalePerfumes = [107, 153, 267, 274];
 
 // get elements
 const ladyPerfumesTable = document.getElementById("lady-perfumes");
@@ -22,8 +22,8 @@ ladyPerfumesTable.innerHTML = "";
 gentPerfumesTable.innerHTML = "";
 
 fetch("perfumes.json")
-    .then((response) => response.json())
-    .then((data) => fillPerfumeListTable(data)); // perfume list object
+  .then((response) => response.json())
+  .then((data) => fillPerfumeListTable(data)); // perfume list object
 
 /*Json file template:
 {
@@ -37,61 +37,61 @@ fetch("perfumes.json")
 
 // fill the perfume list table with data of json file
 function fillPerfumeListTable(perfumeList) {
-    perfumeList.forEach(function (perfume) {
-        let row = document.createElement("tr");
-        let data1 = document.createElement("td"); // perfume number
-        let data2 = document.createElement("td"); // perfume code
-        let data3 = document.createElement("td"); // perfume name
-        let data4 = document.createElement("td"); // fragrance family
+  perfumeList.forEach(function (perfume) {
+    let row = document.createElement("tr");
+    let data1 = document.createElement("td"); // perfume number
+    let data2 = document.createElement("td"); // perfume code
+    let data3 = document.createElement("td"); // perfume name
+    let data4 = document.createElement("td"); // fragrance family
 
-        data1.textContent = perfume.no;
-        data2.textContent = perfume.code;
-        data3.textContent = perfume.inspiredBy;
-        data4.textContent = perfume.fragranceFamily;
+    data1.textContent = perfume.no;
+    data2.textContent = perfume.code;
+    data3.textContent = perfume.inspiredBy;
+    data4.textContent = perfume.fragranceFamily;
 
-        row.appendChild(data1);
-        row.appendChild(data2);
-        row.appendChild(data3);
-        row.appendChild(data4);
+    row.appendChild(data1);
+    row.appendChild(data2);
+    row.appendChild(data3);
+    row.appendChild(data4);
 
-        // mark the actual row
-        newPerfumes.forEach((perfNo) => {
-            if (perfume.no == perfNo) {
-                row.classList.add("new");
-                row.setAttribute("title", "This perfume is a New Arrival.");
-            }
-        });
-        onSalePerfumes.forEach((perfNo) => {
-            if (perfume.no == perfNo) {
-                row.classList.add("sale");
-                row.setAttribute("title", "This perfume is on sale.");
-            }
-        });
-        unavailablePerfumes.forEach((perfNo) => {
-            if (perfume.no == perfNo) {
-                row.className = ""; //clear all other class names (because interference)
-                row.classList.add("unavailable");
-                row.setAttribute("title", "This perfume Temporarily does not available.");
-            }
-        });
-
-        if (perfume.gender === "female") {
-            ladyPerfumesTable.appendChild(row);
-        } else if (perfume.gender === "male") {
-            gentPerfumesTable.appendChild(row);
-        } else {
-            alert("Unexpected error....");
-        }
+    // mark the actual row
+    newPerfumes.forEach((perfNo) => {
+      if (perfume.no == perfNo) {
+        row.classList.add("new");
+        row.setAttribute("title", "This perfume is a New Arrival.");
+      }
     });
+    onSalePerfumes.forEach((perfNo) => {
+      if (perfume.no == perfNo) {
+        row.classList.add("sale");
+        row.setAttribute("title", "This perfume is on sale.");
+      }
+    });
+    unavailablePerfumes.forEach((perfNo) => {
+      if (perfume.no == perfNo) {
+        row.className = ""; //clear all other class names (because interference)
+        row.classList.add("unavailable");
+        row.setAttribute("title", "This perfume Temporarily does not available.");
+      }
+    });
+
+    if (perfume.gender === "female") {
+      ladyPerfumesTable.appendChild(row);
+    } else if (perfume.gender === "male") {
+      gentPerfumesTable.appendChild(row);
+    } else {
+      alert("Unexpected error....");
+    }
+  });
 }
 
 // unhide the correspondent legend if necessary
 if (unavailablePerfumes.length > 0) {
-    legendUnavailable.classList.remove("hide");
+  legendUnavailable.classList.remove("hide");
 }
 if (newPerfumes.length > 0) {
-    legendNew.classList.remove("hide");
+  legendNew.classList.remove("hide");
 }
 if (onSalePerfumes.length > 0) {
-    legendOnSale.classList.remove("hide");
+  legendOnSale.classList.remove("hide");
 }
